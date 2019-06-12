95b19ce46eccab90b8ff1f588b77a376bc85f468
eting83646 <48427534+eting83646@users.noreply.github.com>
2019-06-12 14:15:23 +0800

    Add files via upload

--- src/Home.js
+++ src/Home.js
@@ -1,62 +1,108 @@
-import React from 'react';
-import {Link} from "react-router-dom";
-import Button from '@material-ui/core/Button';
-import Paper from '@material-ui/core/Paper';
-require('./style1.css');
+import React, { Component } from "react";
+import { Layout, Carousel } from "antd";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  withRouter
+} from "react-router-dom";
+import "./App.css";
+import Button from "@material-ui/core/Button";
+import Paper from "@material-ui/core/Paper";
+import Catalog from "./Catalog";
+import ShoppingList from "./Shoppinglist";
+import BookPage from "./Book";
+import OrderPage from "./Order";
+import ItemPage from "./OrderItem";
+import Commentfunct from "./Comment/Comment";
 
-class Home extends React.Component{    
-    render(){            
-        return(                
-        <div>                    
-            <Paper id="top_right">
-                <Link to="/Home" ><Button>好书强推</Button></Link>|
-                <Link to="/Manage" ><Button>订单管理</Button></Link>|
-                <Link to="/BookList" ><Button>书籍浏览</Button></Link>|
-                <Link to="/" ><Button>注销</Button></Link>
-            </Paper><br/>
-            <div class="clear"></div>
-		    <div id="menu">
-                <Link to="/Home" >图书总榜</Link>
-			    <Link to="/Home" >儿童</Link>
-			    <Link to="/Home" >文学</Link>
-			    <Link to="/Home" >进口原版</Link>
-			    <Link to="/Home" >小说</Link>
-			    <Link to="/Home" >历史</Link>
-			    <Link to="/Home" >经济管理</Link>
-            </div>
-            <div class="book">
-                <div class="bookimg">
-                <img src={require('./img/1.jpg')} /> 
-            </div>
-                <div class="bookIntr">
-                    <span>书名：</span>
-                    <span>三体</span><br/>
-                    <span>售价：40</span>
-            </div>
-            </div>
-            <div class="book">
-                <div class="bookimg">
-                    <img src={require('./img/2.jpg')}  /> 
-                </div>
-                <div class="bookIntr">
-                    <span>书名：</span>
-                    <span>流浪地球</span><br/>
-                    <span>售价：50</span>
-                </div>
-            </div>
-            <div id="jumpPage">
-                <Link to="/Home" >上一页</Link>
-                <Link to="/Home" >1</Link>
-                <Link to="/Home" >2</Link>
-                <Link to="/Home" >3</Link>
-                <Link to="/Home" >4</Link>
-                <Link to="/Home" >5</Link>
-                <Link to="/Home" >6</Link>
-                <Link to="/Home" >7</Link>
-                <Link to="/Home" >下一页</Link>
-            </div>              
-    </div>            
-    );        
-    }
+const { Content } = Layout;
+class HomePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bookToDisplay: {},
+      orderToDisplay: 0,
+      currentUserId: this.props.userid
+    };
+  }
+  jumpToDetail = book => {
+    this.setState({ bookToDisplay: book });
+  };
+  showOrderItems = orderid => {
+    this.setState({ orderToDisplay: orderid });
+  };
+
+  Logout = () => {
+    alert("安全退出");
+    this.props.history.push("/Login");
+  };
+  render() {
+    return (
+      <Router>
+        <div>
+          <Paper>
+            <Link to="/Home">
+              <Button>主页</Button>
+            </Link>
+            <Link to="/catalog">
+              <Button>书籍浏览</Button>
+            </Link>
+            <Link to="/mycart">
+              <Button>购物车</Button>
+            </Link>
+            <Link to="/order">
+              <Button>订单管理</Button>
+            </Link>
+            <Link to="/comment">
+              <Button>問答</Button>
+            </Link>
+            <Button onClick={this.Logout}>注销</Button>
+          </Paper>
+          <Content>
+            <Route
+              path="/Home"
+              render={() => (
+                <Carousel autoplay class="slick-slide">
+                  <h1> Welcome to bookstore!</h1>
+                </Carousel>
+              )}
+            />
+
+            <Route
+              path="/catalog"
+              render={() => <Catalog showDetail={this.jumpToDetail} />}
+            />
+            <Route
+              path="/comment"
+              render={() => <Commentfunct showDetail={this.jumpToDetail} />}
+            />
+            <Route
+              path="/mycart"
+              render={() => <ShoppingList uID={this.state.currentUserId} />}
+            />
+            <Route
+              path="/bookpage"
+              render={() => <BookPage book={this.state.bookToDisplay} />}
+            />
+            <Route
+              path="/order"
+              render={() => (
+                <OrderPage
+                  showDetail={this.showOrderItems}
+                  judge={this.state.currentUserId}
+                />
+              )}
+            />
+            <Route
+              path="/orderitempage"
+              render={() => <ItemPage order={this.state.orderToDisplay} />}
+            />
+          </Content>
+        </div>
+      </Router>
+    );
+  }
 }
-export default Home;
+
+export default withRouter(HomePage);
